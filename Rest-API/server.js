dec9e9ecc578107df12dace203d8add56a8e2a06
hisamc91237 <hisamc2000@gmail.com>
2026-07-02 07:40:54 +0530

    Folder structure, Delete and patch Method

--- Rest-API/server.js
+++ Rest-API/server.js
@@ -1,22 +1,5 @@
-const express = require("express");
-
-const app = express();
-
-app.use(express.json()); // Middleware - use to read data in req.body
-
-const notes = [];
-
-// PUT Method
-app.post("/notes", (req, res) => {
-  console.log(req.body);
-  notes.push(req.body);
-  res.send("Notes created");
-});
-
-app.get("/notes", (req, res) => {
-  res.send(notes);
-});
+const app = require("./src/app");
 
 app.listen(3000, () => {
-  console.log("Server is running....");
+  console.log("Server is running...");
 });
