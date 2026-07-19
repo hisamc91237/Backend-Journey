15f3427d8567623ff71386a3aabb7e8c877b058e
hisamc91237 <hisamc2000@gmail.com>
2026-07-19 21:21:33 +0530

    Cookie Storage

--- JWT/routes/auth.route.js
+++ JWT/routes/auth.route.js
@@ -1,20 +1,39 @@
 const express = require("express");
 const userModel = require("../models/user.model");
-
+const jwt = require("jsonwebtoken");
 const authRouter = express.Router();
 
 // POST /api/auth/register
 authRouter.post("/register", async (req, res) => {
   const { email, name, password } = req.body;
 
+  const isUserAlreadyExist = await userModel.findOne({ email });
+
+  if (isUserAlreadyExist) {
+    return res.status(409).json({
+      message: "user already exists with this email",
+    });
+  }
+
   const user = await userModel.create({
     email,
     name,
     password,
   });
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("jwt_token", token);
+
   res.status(201).json({
     message: "user registered",
     user,
+    token,
   });
 });
 
--- JWT/src/app.js
+++ JWT/src/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const authRouter = require("../routes/auth.route");
+const cookieParser = require("cookie-parser");
 
 const app = express();
+app.use(cookieParser()); // Midcleware to use Cookie Storage
 
 app.use(express.json()); // Middleware
 
